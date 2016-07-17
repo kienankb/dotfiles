01bd4922260b4781ebef2c4d350bbdd335e8d115
Kienan Knight-Boehm <kienan@kienankb.com>
2016-07-16 21:57:50 -0700

    Added git config, started config file placement

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 LIBS="xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev"
 BASICS="gcc apt-file"
 
+git config --global user.name "Kienan Knight-Boehm"
+git config --global user.email "kienan@kienankb.com"
+git config --global push.default simple
+
 echo "Updating sources..."
 sudo apt-get update
 echo "Installing needed libraries..."
@@ -19,3 +23,12 @@ cd bspwm
 make && sudo make install
 cd ../sxhkd
 make && sudo make install
+
+cd ..
+
+echo "Placing config files..."
+mkdir ~/.config ~/.config/bspwm ~/.config/sxhkd
+cp configs/bspwmrc ~/.config/bspwm/
+chmod +x configs/bspwm/bspwmrc
+cp configs/sxhkdrc ~/.config/sxhkd/
+chmod +x configs/sxhkd/sxhkdrc

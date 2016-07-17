80480811c0ae78d65c7db35e3c72cec0e0326bb6
Kienan Knight-Boehm <kienan@kienankb.com>
2016-07-16 23:12:43 -0700

    Added panel scripts

--- install.sh
+++ install.sh
@@ -37,14 +37,21 @@ cd ../bar
 make && sudo make install
 cd ..
 
-echo "Placing config files..."
+echo "Placing config files and scripts..."
+# bspwm, sxhkd configs
 mkdir -p ~/.config/bspwm ~/.config/sxhkd
 cp configs/bspwmrc ~/.config/bspwm/
 chmod +x ~/.config/bspwm/bspwmrc
 cp configs/sxhkdrc ~/.config/sxhkd/
 chmod +x ~/.config/sxhkd/sxhkdrc
+# xinitrc
 cp configs/.xinitrc ~/
 chmod +x ~/.xinitrc
+# panel fifo; if statement is mostly for testing
 if ! grep -q "PANEL_FIFO" /etc/profile ; then
 	echo "export PANEL_FIFO='/tmp/panel-fifo'" | sudo tee -a /etc/profile
 fi
+# all the scripts you could ever want
+mkdir -p ~/.scripts
+cp configs/scripts/* ~/.scripts
+chmod +x ~/.scripts/panel ~/.scripts/panel_bar

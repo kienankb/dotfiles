167aa165b0af14aabcc158beebb9a0d6eb6a4cff
Kienan Knight-Boehm <kienan@kienankb.com>
2016-07-18 00:26:29 -0700

    Added ncmpcpp and mpd to basics installs

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LIBS="xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxft-dev libX11-xcb-dev libxcb-xtest0-dev libxinerama-dev"
-BASICS="gcc apt-file xorg fonts-inconsolata htop colortest screenfetch screen tmux asciinema lolcat cowsay fortune feh scrot xbacklight colordiff alsa-utils"
+BASICS="gcc apt-file xorg fonts-inconsolata htop colortest screenfetch screen tmux asciinema lolcat cowsay fortune feh scrot xbacklight colordiff alsa-utils ncmpcpp mpd"
 
 rm install.log
 

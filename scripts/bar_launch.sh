0dc4b8f720c2b873a3392d00604a2d97dd98dce0
Kienan Knight-Boehm <kienan@kienankb.com>
2016-10-31 18:57:58 -0700

    Re-did status bar, looks better now

--- scripts/bar_launch.sh
+++ scripts/bar_launch.sh
@@ -2,17 +2,36 @@
 
 . bar_vars.sh
 
-if [[ `hostname` == "friday" ]]; then
-	bspc subscribe			| python ~/.scripts/bar_dm.py | lemonbar -p -d -B#00000000 -F$PANEL_TEXT_COLOR -a 1 -g 75x20+0+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" &
-	bar_datetime.sh			| lemonbar -p -d -B$PANEL_BKGND_COLOR -F$PANEL_TEXT_COLOR -a 1 -g 175x20+75+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" &
-	bar_battery.sh			| lemonbar -p -d -B$PANEL_BKGND_COLOR -F$PANEL_TEXT_COLOR -a 1 -g 50x20+250+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" &
-	bar_disk.sh /dev/sda6	| lemonbar -p -d -B$PANEL_BKGND_COLOR -F$PANEL_TEXT_COLOR -a 1 -g 50x20+300+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" &
-	bar_volume.sh			| lemonbar -p -d -B$PANEL_BKGND_COLOR -F$PANEL_TEXT_COLOR -a 1 -g 50x20+350+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" &
-	bar_shortcuts.sh		| lemonbar -p -d -B$PANEL_BKGND_COLOR -F$PANEL_TEXT_COLOR -a 3 -g 50x20+400+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" | bash &
-	bar_power.sh			| lemonbar -p -d -B$PANEL_BKGND_COLOR -F$PANEL_TEXT_COLOR -a 2 -g 50x20+450+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" | sudo bash &
-elif [[ `hostname` == "tuesday" ]]; then
-	bspc subscribe			| python ~/.scripts/bar_dm.py | lemonbar -p -d -B#00000000 -F$PANEL_TEXT_COLOR -a 1 -g 150x20+0+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" &
-	bar_datetime.sh			| lemonbar -p -d -B$PANEL_BKGND_COLOR -F$PANEL_TEXT_COLOR -a 1 -g 200x20+150+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" &
-	bar_disk.sh /dev/sdb1	| lemonbar -p -d -B$PANEL_BKGND_COLOR -F$PANEL_TEXT_COLOR -a 1 -g 50x20+350+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" &
-	bar_volume.sh			| lemonbar -p -d -B$PANEL_BKGND_COLOR -F$PANEL_TEXT_COLOR -a 1 -g 50x20+400+5 -f "FiraMono:size=9:antialias=true" -f "icomoon:size=10" &
+if [[ `cat /etc/hostname` == "friday" ]]; then
+
+	echo " " | \
+		lemonbar -p -d -B$MAIN_BG -g 1600x25+0+0 &
+	bspc subscribe | python ~/.scripts/bar_dm.py | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -g 70x25+0+0 &
+	bar_datetime.sh | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -f "icomoon:size=$ICO_SIZE" -g 175x25+70+0 &
+	bar_battery.sh | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -f "icomoon:size=$ICO_SIZE" -g 50x25+245+0 &
+	bar_disk.sh /dev/sda6 | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -f "icomoon:size=$ICO_SIZE" -g 50x25+295+0 &
+	bar_volume.sh | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -f "icomoon:size=$ICO_SIZE" -g 50x25+345+0 &
+	bar_shortcuts.sh | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -f "icomoon:size=$ICO_SIZE" -g 50x25+395+0 -a 3 | bash &
+
+elif [[ `cat /etc/hostname` == "tuesday" ]]; then
+
+	echo " " | \
+		lemonbar -p -d -B$MAIN_BG -g 1920x25+0+0 &
+	bspc subscribe | python ~/.scripts/bar_dm.py | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -g 145x20+5+5 &
+	bar_datetime.sh | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -f "icomoon:size=$ICO_SIZE" -g 200x25+150+5 &
+	bar_disk.sh /dev/sdb1 | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -f "icomoon:size=$ICO_SIZE" -g 50x25+350+5 &
+	bar_volume.sh | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -f "icomoon:size=$ICO_SIZE" -g 50x25+400+5 &
+	bar_shortcuts.sh | \
+		lemonbar -p -d -B$BAR_BG -F$BAR_FG -f "FiraMono:size=$TXT_SIZE:antialias=true" -f "icomoon:size=$ICO_SIZE" -g 50x25+395+0 -a 3 | bash &
+
 fi
--- scripts/bar_power.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo -e "%{c}%{A:'shutdown -h now':'reboot'}\ue9b6%{A} %{A:'lock.sh':}\ue98f%{A}"

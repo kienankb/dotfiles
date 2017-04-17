67d84940600dad28deb357a26f6ea51a17d9b93e
Kienan Knight-Boehm <kienan@kienankb.com>
2017-04-17 10:27:22 -0400

    Changed install script

--- install.sh
+++ install.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
 LIBS="xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxft-dev libX11-xcb-dev libxcb-xtest0-dev libxinerama-dev"
-BASICS="gcc apt-file xorg fonts-inconsolata htop colortest screenfetch screen tmux asciinema lolcat cowsay fortune feh scrot xbacklight colordiff alsa-utils ncmpcpp mpd compton mc redshift i3lock imagemagick"
+BASICS="make gcc apt-file xorg fonts-inconsolata htop colortest screenfetch screen tmux asciinema lolcat cowsay fortune feh scrot xbacklight colordiff alsa-utils ncmpcpp mpd compton mc redshift i3lock imagemagick"
 
-rm install.log
-
-echo ""
-echo "==========================================="
-echo "YO, Y'ALL BETTER BE RUNNING THIS FROM A TTY"
-echo "==========================================="
-echo ""
-
-echo "Updating sources..."
-sudo apt-get update >> install.log
+sudo apt update & sudo apt upgrade -y
 echo "Installing needed libraries..."
-sudo apt-get install -y $LIBS >> install.log
+sudo aptinstall -y $LIBS
 echo "Installing needed packages..."
-sudo apt-get install -y $BASICS >> install.log
-
-echo ""
-echo "=============================================="
-echo "THINGS YOU STILL NEED TO DO, YO:"
-echo "=============================================="
-echo "* install Sublime (you're on your own)"
-echo "* put any keys in .ssh with the .osk extension"
+sudo apt install -y $BASICS

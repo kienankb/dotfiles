ab8e3e0a3f80ccc2866cb3827d173e931a9b887a
Kienan Knight-Boehm <kienan@kienankb.com>
2018-03-18 00:43:27 -0700

    More postpacstrap tweaks

--- postpacstrap.sh
+++ postpacstrap.sh
@@ -16,7 +16,7 @@ fi
 echo "Generating locales, creating hostname files..."
 ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
 hwclock --systohc
-sed -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo $HOSTNAME > /etc/hostname
@@ -25,7 +25,7 @@ echo "::1\tlocalhost" >> /etc/hosts
 echo "127.0.1.1\t$HOSTNAME.localdomain\t$HOSTNAME" >> /etc/hosts
 
 read -p "Set up wireless networking? (y/n): " -r
-if [[ $REPLY =~[Yy]$ ]]; then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 	pacman -S iw wpa_supplicant
 	echo "ctrl_interface=/run/wpa_supplicant" > /etc/wpa_supplicant/wpa_supplicant.conf
 	echo "ctrl_interface_group=wheel" >> /etc/wpa_supplicant/wpa_supplicant.conf
@@ -42,7 +42,7 @@ pacman -S grub efibootmgr os-prober
 grub-install $DISK
 grub-mkconfig -o /boot/grub/grub.cfg
 read -p "Running an Intel CPU? (y/n): " -r
-if [[ $REPLY =~ [Yy]$ ]]; then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 	pacman -S intel-ucode
 fi
 

841a8d7496b72965ae999477024a6f6480cfce41
Kienan Knight-Boehm <kienan@kienankb.com>
2018-03-22 00:31:56 -0700

    Added aur project cloning; added alsa

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -2,5 +2,9 @@
 
 sudo pacman -S - < ./install/packages		# install everything we want from pacman
 sudo pip install -r ./install/pip-packages	# install everything we want from pip
+mkdir -p ~/projects/aur				# set up aur directory
+while read PKG; do
+	git clone https://aur.archlinux.org/$PKG.git ~/projects/aur/$PKG
+done < install/aur-packages
 rm ~/.bashrc					# stow will fail otherwise
 stow -t ~/ home/				# drop dotfiles into place

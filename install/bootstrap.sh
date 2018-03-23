69f825832eaba41d8b16db1832609e6d06bb7913
Kienan Knight-Boehm <kienan@kienankb.com>
2018-03-22 21:33:38 -0700

    Changed AUR pkgs to git clones w/ other pkgs

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -2,9 +2,15 @@
 
 sudo pacman -S - < ./install/packages		# install everything we want from pacman
 sudo pip install -r ./install/pip-packages	# install everything we want from pip
-mkdir -p ~/projects/aur				# set up aur directory
+mkdir -p ~/projects/tools			# set up aur directory
+cd ~/projects/tools
 while read PKG; do
-	git clone https://aur.archlinux.org/$PKG.git ~/projects/aur/$PKG
-done < install/aur-packages
+	git clone $PKG
+done < install/cloneables
+cd -
 rm ~/.bashrc					# stow will fail otherwise
 stow -t ~/ home/				# drop dotfiles into place
+echo "Things you still have to do:"
+echo "1. Build & install all the things you cloned"
+echo "2. Install sublime-text"
+echo "3. Set up your ssh keys"

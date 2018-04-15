d01066bc0705d8e628d9df9777eee0c6d7091c13
Kienan Knight-Boehm <kienan@kienankb.com>
2018-04-15 00:52:50 -0700

    To hell with flatpak

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -11,8 +11,6 @@ cd -
 rm ~/.bashrc					# stow will fail otherwise
 mkdir ~/.config
 stow -t ~/ home/				# drop dotfiles into place
-sudo flatpak install flathub com.discordapp.Discord
-sudo flatpak install flathub com.spotify.Client
 curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
 echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 sudo pacman -Syu sublime-text

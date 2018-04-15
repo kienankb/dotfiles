30db517da15d5362f64e3da9652e9ccd08db0904
Kienan Knight-Boehm <kienan@kienankb.com>
2018-04-14 22:50:41 -0700

    Updated installation files

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -9,8 +9,15 @@ while read PKG; do
 done < ~/projects/dotfiles/install/cloneables
 cd -
 rm ~/.bashrc					# stow will fail otherwise
+mkdir ~/.config
 stow -t ~/ home/				# drop dotfiles into place
+sudo flatpak install flathub com.discordapp.Discord
+sudo flatpak install flathub com.spotify.Client
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syu sublime-text
 echo "Things you still have to do:"
 echo "1. Build & install all the things you cloned"
-echo "2. Install sublime-text"
+echo "2. Install video drivers"
 echo "3. Set up your ssh keys"
+echo "4. Link st config & sublime configs after installing Package Control, DA UI"

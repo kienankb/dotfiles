2e8b5d366c65bb8bfbe73f1d3b0ba2ea41794c58
Kienan Knight-Boehm <kienan@kienankb.com>
2021-11-13 15:24:33 -0800

    rofi fixes until I get theme config working again

--- home/.scripts/kaomoji.sh
+++ home/.scripts/kaomoji.sh
@@ -2,6 +2,6 @@
 # Modded version of the kaomoji rofi script from:
 #   https://gitlab.com/ceda_ei/kaomoji-rofi
 
-selection=$(rofi -i -dmenu $@ < $(dirname $0)/kaomoji.txt)
+selection=$(rofi -theme ~/.config/rofi/slate-kkb.rasi -i -dmenu $@ < $(dirname $0)/kaomoji.txt)
 kaomoji=$(echo $selection | sed "s|$(echo -e "\ufeff").*||")
 echo -n "$kaomoji" | xclip -selection clipboard
